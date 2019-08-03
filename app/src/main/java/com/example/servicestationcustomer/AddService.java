1f9428e8656a0a215c83f80f37c5af4aa0cb6ba1
bawantharathnayakasliit <bawantharathnayaka@gmail.com>
2019-08-04 00:21:51 +0530

    Update

--- app/src/main/java/com/example/servicestationcustomer/AddService.java
+++ app/src/main/java/com/example/servicestationcustomer/AddService.java
@@ -1,7 +1,9 @@
 package com.example.servicestationcustomer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddService extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class AddService extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service);
     }
+
 }
