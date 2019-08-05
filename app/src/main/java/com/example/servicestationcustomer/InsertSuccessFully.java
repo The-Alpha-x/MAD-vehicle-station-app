e88de353cf1c1fa07f1ad3c756a43e797b4e6402
bawantharathnayakasliit <bawantharathnayaka@gmail.com>
2019-08-05 22:39:31 +0530

    edit activities

--- app/src/main/java/com/example/servicestationcustomer/InsertSuccessFully.java
+++ app/src/main/java/com/example/servicestationcustomer/InsertSuccessFully.java
@@ -1,7 +1,9 @@
 package com.example.servicestationcustomer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InsertSuccessFully extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class InsertSuccessFully extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_success_fully);
+    }
+    public void onButtonClick(View v){
+        Intent myIntent = new Intent(getBaseContext(), AddService.class);
+        startActivity(myIntent);
     }
 }
