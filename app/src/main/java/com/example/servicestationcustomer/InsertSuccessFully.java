001ba956d3a5e77d59be5bd29c43a923db6d190e
oshadiranathungesliit <radoshadi54@gmail.com>
2019-08-05 11:27:02 +0530

    new activity

---
+++ app/src/main/java/com/example/servicestationcustomer/InsertSuccessFully.java
@@ -0,0 +1,13 @@
+package com.example.servicestationcustomer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertSuccessFully extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_success_fully);
+    }
+}
